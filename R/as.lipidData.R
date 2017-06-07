8ce9be048fbd72a8047fd179ab41a30b70fb1a61
Kelly Stratton <kelly.stratton@pnnl.gov>
2017-06-07 15:13:25 -0700

    Update default data_scale argument value for as.() functions

--- R/as.lipidData.R
+++ R/as.lipidData.R
@@ -12,7 +12,7 @@
 #'
 #' @details Objects of class 'lipidData' contain some attributes that are referenced by downstream functions. These attributes can be changed from their default value by manual specification. A list of these attributes as well as their default values are as follows:
 #' \tabular{ll}{
-#' data_scale \tab Scale of the data provided in \code{e_data}. Acceptable values are 'log2', 'log10', 'log', and 'abundance', which indicate data is log base 2, base 10, natural log transformed, and raw abundance, respectively. Default values is 'log2'. \cr
+#' data_scale \tab Scale of the data provided in \code{e_data}. Acceptable values are 'log2', 'log10', 'log', and 'abundance', which indicate data is log base 2, base 10, natural log transformed, and raw abundance, respectively. Default values is 'abundance'. \cr
 #' \tab \cr
 #' data_norm \tab A logical argument, specifying whether the data has been normalized or not. Default value is FALSE. \cr
 #' \tab \cr
@@ -56,7 +56,7 @@ as.lipidData <- function(e_data, f_data, e_meta = NULL, edata_cname, fdata_cname
 
 ## lipid data ##
 .as.lipidData <- function(e_data, f_data, e_meta = NULL, edata_cname, fdata_cname,
-                          emeta_cname = NULL, data_scale = "log2",
+                          emeta_cname = NULL, data_scale = "abundance",
                           data_norm = FALSE, norm_info=NULL,
                           data_types=NULL){
 
