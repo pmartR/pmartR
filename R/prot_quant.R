0127962574ba2101514125b25cd388f739f30edd
godi678 <iobani.godinez@pnnl.gov>
2017-07-06 09:27:26 -0700

    foreach statement changes

--- R/prot_quant.R
+++ R/prot_quant.R
@@ -16,51 +16,28 @@ prot_quant<- function(pepData, method, proteoformRes, parallel = TRUE){
   edata_cname_id<- which(names(pepData$e_data) == edata_cname)
   
   if(method == 'rrollup'){
-    
+
     library(doParallel)
     cl<- makeCluster(4)
     registerDoParallel(cl)
     
-   res<- vector("list", length(proteoformRes))
-    
-    r<-foreach(i=1:length(proteoformRes),.export = c("rrollup_r")) %dopar%{
-      
-      rollup_res<- list()
-      column<- c()
-      pformRes_sub<- proteoformRes[[i]]
-      pformRes_sub<- as.data.frame(pformRes_sub)
-      
-      prot_name<- pformRes_sub$Protein[1]
-      
-      max_proteo_id<- max(pformRes_sub$proteoformID)
-      
-      for(j in 1:max_proteo_id){
-        cur_subset<- pformRes_sub[which(pformRes_sub$proteoformID == j), ]
-        
-        inds<- which(pepData$e_data[[edata_cname]] %in% cur_subset[[edata_cname]])
-        
-        edata_subset<- pepData$e_data[inds, ]
-        edata_subset<- edata_subset[, -edata_cname_id]
-        column[j]<- paste(prot_name, j, sep=';')
-        
-        result<- rrollup_r(edata_subset)
-        rollup_res[[j]]<- result
-        
+    final_res<- foreach(i=1:length(proteoformRes), .combine = 'rbind', .packages="foreach") %dopar%{
+        pformRes_sub<- proteoformRes[[i]]
+          
+        foreach(j = 1:max(pformRes_sub$proteoformID), .combine = 'rbind') %do%{
+           prot_name<- pformRes_sub$Protein[1]
+           cur_subset<- pformRes_sub[which(pformRes_sub$proteoformID == j), ]
+           inds<- which(pepData$e_data[[edata_cname]] %in% cur_subset[[edata_cname]])
+           edata_subset<- pepData$e_data[inds, -edata_cname_id]
+           prot<- paste(prot_name, j, sep=';')
+           result<- rrollup_r(edata_subset)
+           result<- result[,-1]
+           result<- cbind(prot, result)
       }
-      
-      rollup_res<-do.call(rbind, rollup_res)
-      rollup_res<- rollup_res[, -1]
-      rollup_res<- cbind(column, rollup_res)
-      names(rollup_res)[1]<- emeta_cname
-      
-      
-        res[[i]]<- rollup_res
-      
     }
-    
     stopCluster(cl)
     
-    final_res<- do.call(rbind, r)
+    names(final_res)[1]<- emeta_cname
     proteoformRes_sub<-attr(proteoformRes, "proteoformRes_subset")
     
     #proData_object<- as.proData(e_data = final_res, f_data= f_data, e_meta= NULL, edata_cname = emeta_cname, fdata_cname = fdata_cname)
