6b11092c349cf9dd9339f5cd0d4a972fc847d6f6
godi678 <iobani.godinez@pnnl.gov>
2017-07-11 16:16:25 -0700

    updating attributes in pquant funcs

--- R/pquant_mean.R
+++ R/pquant_mean.R
@@ -37,9 +37,16 @@ pquant_mean <- function(pepData){
   else {e_meta = pepData$e_meta[,-which(names(pepData$e_meta)==pep_id)]} 
   
   
-  prodata = as.proData(e_data = data.frame(res, check.names=FALSE), f_data = pepData$f_data, e_meta = NULL , fdata_cname = samp_id, edata_cname = pro_id, data_scale = data_scale, data_norm = data_norm)
+  prodata = as.proData(e_data = data.frame(res, check.names=FALSE), f_data = pepData$f_data,  e_meta = e_meta ,edata_cname = pro_id, fdata_cname = samp_id, emeta_cname = pro_id, data_scale = data_scale, data_norm = data_norm)
   
-  attr(prodata, "meta_info") = attr(pepData, "meta_info")
+  #updating prodata attributes
+  attr(prodata, "data_info")$norm_info = attr(pepData, "data_info")$norm_info
+  attr(prodata, "data_info")$data_types = attr(pepData, "data_info")$data_types
+  attr(prodata, "data_info")$norm_method = attr(pepData, "data_info")$norm_method
+  
+  attr(prodata, "filters")<- attr(pepData, "filters")
+  attr(prodata, "group_DF")<- attr(pepData, "group_DF")
+  attr(prodata, "imdanova")<- attr(pepData, "imdanova")
   
   
   return(prodata)
