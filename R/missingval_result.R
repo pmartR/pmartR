b25ce537c924047d506d32f2319ab606109aa98a
godi678 <iobani.godinez@pnnl.gov>
2017-08-09 17:44:27 -0700

    missingval_result function

---
+++ R/missingval_result.R
@@ -0,0 +1,30 @@
+
+
+missingval_result<- function(omicsData){
+  
+  #check for correct class
+  if(!(class(omicsData) %in% c("pepData", "proData", "lipidData", "metabData" ))) stop("omicsData must have class of the following, 'pepData', 'proData', 'lipidData', 'metabData'")
+  
+  
+  #pulling cname attr
+  edata_cname<- attr(omicsData, "cnames")$edata_cname
+  edata_cname_id<- which(names(omicsData$e_data)== edata_cname)
+  fdata_cname<- attr(omicsData, "cnames")$fdata_cname
+  
+  #we'll count the number of NA values per column
+  temp<- omicsData$e_data[, -edata_cname_id]
+  na_per_col<- apply(is.na(temp), 2, sum)
+  
+  na_by_sample<- data.frame("sample_names" = names(temp), "num_NA" = as.numeric(na_per_col))
+  names(na_by_sample)[1]<- fdata_cname
+  
+  #now count the number of NA values per row
+  na_per_row<- apply(is.na(temp), 1, sum)
+  na_by_molecule<- data.frame("molecule"= omicsData$e_data[,edata_cname_id], "num_NA"= as.numeric(na_per_row))
+  names(na_by_molecule)[1]<- edata_cname
+  result<- list(na_by_sample, na_by_molecule)
+  class(result)<- "NARes"
+
+  return(result)  
+  
+}
