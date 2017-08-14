25b36a15e723f5ded2a21c046d3488d095c0b02b
godi678 <iobani.godinez@pnnl.gov>
2017-08-14 15:31:04 -0700

    made changes to plot.naRes function

--- R/plot_missingval.R
+++ R/plot_missingval.R
@@ -3,17 +3,36 @@
 #' For plotting an S3 object of type 'naRes':
 #' 
 #'@rdname plot-naRes
+#'@param type is for specifying plot type, there are three options, "bySample", "byMolecule" and "Both"
+#' \tabular{ll}{
+#' \code{x_lab} \tab character string to be used for x-axis label. Defaults to NULL \cr
+#' \code{y_lab} \tab character string to be used for y-axis label. Defaults to NULL \cr
+#' \code{title_plot} \tab character string to be used for the plot title. Defaults to NULL. \cr
+#' \code{title_size} \tab integer value specifying the font size for the plot title. Default is 14. \cr
+#' \code{x_lab_size} \tab integer value indicating the font size for the x-axis. Defaults to 11. \cr
+#' \code{y_lab_size} \tab integer value indicating the font size for the y-axis. Defaults to 11. \cr
+#' \code{bar_width} \tab integer value indicating the bar width in a barplot (when type = "bySample"). Defaults to .8. \cr
+#' \code{binwidth} \tab integer value indicating the bin width in a histogram (when type = "byMolecule"). Defaults to 1. \cr
+#' \code{bw_theme} \tab logical indicator of whether to use the "theme_bw". Defaults to FALSE, in which case the ggplot2 default theme is used. \cr
+#' }
+#'
 #'@export
 #'
 
-plot.naRes <- function(naRes_object, x_lab = NULL, ...) {
-  .plot.naRes(naRes_object, x_lab, ...)
+plot.naRes <- function(naRes_object, type, x_lab = NULL, ...) {
+  .plot.naRes(naRes_object, type, x_lab, ...)
 }
 
-.plot.naRes<- function(naRes_object, x_lab = NULL, y_lab = NULL, title_plot = NULL, title_size = 14, x_lab_size = 11, y_lab_size = 11, bw_theme = FALSE){
+.plot.naRes<- function(naRes_object, type, x_lab = NULL, y_lab = NULL, title_plot = NULL, title_size = 14, x_lab_size = 11, y_lab_size = 11, bar_width = .8, binwidth = 1, bw_theme = FALSE){
  
    # check for a naRes object #
   if(class(naRes_object)[1] != "naRes") stop("object must be of class 'naRes'")
+  
+  #check that type is one of the three options
+  if(!(type %in% c("bySample", "byMolecule", "Both"))) stop("type must be one of 'bySample', 'byMolecule', 'Both'")
+  
+  #check that if type "Both" is specified, then x_lab, y_lab and tile_plot are all NULL
+  if(type == "Both" & (!is.null(x_lab)|!is.null(y_lab)|!is.null(title_plot))) stop("if type 'Both' is specified, x_lab, y_lab and title_plot cannot be specified")
   
   if(!is.null(title_plot)) {
     if(!is.character(title_plot)) stop("title_plot must be a character vector")
@@ -32,46 +51,113 @@ plot.naRes <- function(naRes_object, x_lab = NULL, ...) {
   edata_cname<- attr(naRes_object, "cnames")$edata_cname
   fdata_cname<- attr(naRes_object, "cnames")$fdata_cname
   
-  
-  # make labels #
-  xlabel <- ifelse(is.null(x_lab), "Sample Name", x_lab)
-  ylabel <- ifelse(is.null(y_lab), "Missing values (count)", y_lab)
-  plot_title <- ifelse(is.null(title_plot), "Missing Values by Sample", title_plot)
-  
-  #plots NA per sample
- if(bw_theme == FALSE){
-   p<- ggplot2::ggplot(data=na.by.sample, aes(x=na.by.sample[[fdata_cname]], y = na.by.sample$num_NA)) + 
-    ggplot2::geom_bar(stat = "identity", width = .8, color= "black", fill = "steelblue") +
-    ggplot2::geom_text(aes(label = num_NA), vjust = 2, color = "white", size = 3) +
-   
-    ggplot2::xlab(xlabel) +
-    ggplot2::ylab(ylabel) +
-    ggplot2::ggtitle(plot_title) +
-    ggplot2::theme(plot.title = ggplot2::element_text(size = title_size), axis.title.x = ggplot2::element_text(size = x_lab_size), axis.title.y = ggplot2::element_text(size = y_lab_size))
- }
-  else{p<- ggplot2::ggplot(data=na.by.sample, aes(x=na.by.sample[[fdata_cname]], y = na.by.sample$num_NA)) + 
-    ggplot2::geom_bar(stat = "identity", width = .8, color= "black", fill = "steelblue") +
-    ggplot2::geom_text(aes(label = num_NA), vjust = 2, color = "white", size = 3) +
-    bw_theme() +
+  if(type == "bySample"){
+    # make labels #
+    xlabel <- ifelse(is.null(x_lab), "Sample Name", x_lab)
+    ylabel <- ifelse(is.null(y_lab), "Missing values (count)", y_lab)
+    plot_title <- ifelse(is.null(title_plot), "Missing Values by Sample", title_plot)
     
-    ggplot2::xlab(xlabel) +
-    ggplot2::ylab(ylabel) +
-    ggplot2::ggtitle(plot_title) +
-    ggplot2::theme(plot.title = ggplot2::element_text(size = title_size), axis.title.x = ggplot2::element_text(size = x_lab_size), axis.title.y = ggplot2::element_text(size = y_lab_size))
+    #plots NA per sample
+    if(bw_theme == FALSE){
+      p<- ggplot2::ggplot(data=na.by.sample, aes(x=na.by.sample[[fdata_cname]], y = na.by.sample$num_NA)) + 
+          ggplot2::geom_bar(stat = "identity", width = bar_width, color= "black", fill = "steelblue") +
+          ggplot2::geom_text(aes(label = num_NA), vjust = 2, color = "white", size = 3) +
+        
+          ggplot2::xlab(xlabel) +
+          ggplot2::ylab(ylabel) +
+          ggplot2::ggtitle(plot_title) +
+          ggplot2::theme(plot.title = ggplot2::element_text(size = title_size), axis.title.x = ggplot2::element_text(size = x_lab_size), axis.title.y = ggplot2::element_text(size = y_lab_size))
+    }
+    else{
+      p<- ggplot2::ggplot(data=na.by.sample, aes(x=na.by.sample[[fdata_cname]], y = na.by.sample$num_NA)) + 
+          ggplot2::geom_bar(stat = "identity", width = bar_width, color= "black", fill = "steelblue") +
+          ggplot2::geom_text(aes(label = num_NA), vjust = 2, color = "white", size = 3) +
+          theme_bw() +
+      
+          ggplot2::xlab(xlabel) +
+          ggplot2::ylab(ylabel) +
+          ggplot2::ggtitle(plot_title) +
+          ggplot2::theme(plot.title = ggplot2::element_text(size = title_size), axis.title.x = ggplot2::element_text(size = x_lab_size), axis.title.y = ggplot2::element_text(size = y_lab_size))
+    }
+    return(p)
+  }
+ 
+  else if(type == "byMolecule"){
+    # make labels #
+    xlabel <- ifelse(is.null(x_lab), "Number of NA values per Molecule", x_lab)
+    ylabel <- ifelse(is.null(y_lab), "Molecules (count)", y_lab)
+    plot_title <- ifelse(is.null(title_plot), "Missing Values per Molecule", title_plot)
+  
+    #plots NA per molecule
+    if(bw_theme == FALSE){
+      p<-  ggplot2::ggplot(data= na.by.molecule, aes(x = na.by.molecule$num_NA)) +
+           ggplot2::geom_histogram(binwidth = binwidth, aes(fill = ..count..)) + 
+         
+           ggplot2::xlab(xlabel) +
+           ggplot2::ylab(ylabel) +
+           ggplot2::ggtitle(plot_title) +
+           ggplot2::theme(plot.title = ggplot2::element_text(size = title_size), axis.title.x = ggplot2::element_text(size = x_lab_size), axis.title.y = ggplot2::element_text(size = y_lab_size))  
+      }
+    else{
+      p<-  ggplot2::ggplot(data= na.by.molecule, aes(x = na.by.molecule$num_NA)) +
+           ggplot2::geom_histogram(binwidth = binwidth, aes(fill = ..count..)) +      
+           theme_bw() +
+      
+           ggplot2::xlab(xlabel) +
+           ggplot2::ylab(ylabel) +
+           ggplot2::ggtitle(plot_title) +
+           ggplot2::theme(plot.title = ggplot2::element_text(size = title_size), axis.title.x = ggplot2::element_text(size = x_lab_size), axis.title.y = ggplot2::element_text(size = y_lab_size))
+    }
+    return(p)
   }
   
-  
-
-  
-  return(p)
-
-}
-  #plots NA per molecule
- # g<- ggplot2::ggplot(data= na.by.molecule, aes(x = na.by.molecule$num_NA)) +
- #   ggplot2::geom_histogram(binwidth = 1, aes(fill = ..count..)) + 
- #   ggplot2::xlab("Number of NA values per Molecule") +
- #   ggplot2::ylab("Molecules (count)") +
- #   ggplot2::ggtitle("Missing Values per Molecule") +
- #   ggplot2::theme(plot.title = ggplot2::element_text(size = 10), axis.title.x = ggplot2::element_text(size = 10), axis.title.y = ggplot2::element_text(size = 10))
+  #if type = "Both"
+  else{
+    #plots NA per sample
+    if(bw_theme == FALSE){
+      s<- ggplot2::ggplot(data=na.by.sample, aes(x=na.by.sample[[fdata_cname]], y = na.by.sample$num_NA)) + 
+          ggplot2::geom_bar(stat = "identity", width = bar_width, color= "black", fill = "steelblue") +
+          ggplot2::geom_text(aes(label = num_NA), vjust = 2, color = "white", size = 3) +
+        
+          ggplot2::xlab("Sample Name") +
+          ggplot2::ylab("Missing values (count)") +
+          ggplot2::ggtitle("Missing Values by Sample") +
+          ggplot2::theme(plot.title = ggplot2::element_text(size = title_size), axis.title.x = ggplot2::element_text(size = x_lab_size), axis.title.y = ggplot2::element_text(size = y_lab_size))
+    }
+    else{
+      s<- ggplot2::ggplot(data=na.by.sample, aes(x=na.by.sample[[fdata_cname]], y = na.by.sample$num_NA)) + 
+          ggplot2::geom_bar(stat = "identity", width = bar_width, color= "black", fill = "steelblue") +
+          ggplot2::geom_text(aes(label = num_NA), vjust = 2, color = "white", size = 3) +
+          theme_bw() +
+        
+          ggplot2::xlab("Sample Name") +
+          ggplot2::ylab("Missing values (count)") +
+          ggplot2::ggtitle("Missing Values by Sample") +
+          ggplot2::theme(plot.title = ggplot2::element_text(size = title_size), axis.title.x = ggplot2::element_text(size = x_lab_size), axis.title.y = ggplot2::element_text(size = y_lab_size))
+    }
     
-  
+    #plots NA per molecule
+    if(bw_theme == FALSE){
+      m<-  ggplot2::ggplot(data= na.by.molecule, aes(x = na.by.molecule$num_NA)) +
+           ggplot2::geom_histogram(binwidth = binwidth, aes(fill = ..count..)) + 
+        
+           ggplot2::xlab("Number of NA values per Molecule") +
+           ggplot2::ylab("Molecules (count)") +
+           ggplot2::ggtitle("Missing Values per Molecule") +
+           ggplot2::theme(plot.title = ggplot2::element_text(size = title_size), axis.title.x = ggplot2::element_text(size = x_lab_size), axis.title.y = ggplot2::element_text(size = y_lab_size))  
+    }
+    else{
+      m<-  ggplot2::ggplot(data= na.by.molecule, aes(x = na.by.molecule$num_NA)) +
+           ggplot2::geom_histogram(binwidth = binwidth, aes(fill = ..count..)) +      
+           theme_bw() +
+        
+           ggplot2::xlab("Number of NA values per Molecule") +
+           ggplot2::ylab("Molecules (count)") +
+           ggplot2::ggtitle("Missing Values per Molecule") +
+           ggplot2::theme(plot.title = ggplot2::element_text(size = title_size), axis.title.x = ggplot2::element_text(size = x_lab_size), axis.title.y = ggplot2::element_text(size = y_lab_size))  
+       }
+    
+     grid.arrange(s, m, ncol = 2)
+    
+  }
+}
