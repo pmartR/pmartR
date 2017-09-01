55e87f8d7ac62060e472448036303c0ef0b1d1f3
godi678 <iobani.godinez@pnnl.gov>
2017-09-01 14:17:07 -0700

    added missingval_heatmapplot function

---
+++ R/missingval_heatmapplot.R
@@ -0,0 +1,23 @@
+
+#here we will make a temporary function called missingval_heatmapplot, it takes in omicsData and a missingval_result object
+#and returns a heatmap of omicsData$e_data
+
+missingval_heatmapplot<- function(omicsData, missingval_res){
+ 
+by_molecule<- missingval_res$na.by.molecule
+by_molecule<- by_molecule[order(by_molecule$num_NA),]
+
+edata_cname<- attr(omicsData, "cnames")$edata_cname 
+
+e_data<- omicsData$e_data
+
+edata_melt<- melt(e_data, id.vars = edata_cname)
+edata_melt[[edata_cname]]<- factor(edata_melt[[edata_cname]], levels = rev(by_molecule[[edata_cname]]))  
+names(edata_melt)[1]<- "edata_cname"
+
+p <- ggplot(edata_melt, aes(x=variable, y = edata_cname)) + geom_tile(aes(fill = value), colour = "white") + 
+     theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) + ylab("Molecule")
+  
+return(p)
+
+}
