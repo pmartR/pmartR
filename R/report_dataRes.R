aadbaef5f9ead2d0e27f0231e25186f4d58235bd
godi678 <iobani.godinez@pnnl.gov>
2017-12-06 11:29:05 -0800

    adding more checks to report_dataRes

--- R/report_dataRes.R
+++ R/report_dataRes.R
@@ -4,6 +4,7 @@
 #'
 #' @param statRes an object of the class 'statRes', created by \code{\link{summarize}}.
 #' @param minmax logical, for specifying whether or not to include minimum and maximum data in the returned data frame. Defaults to FALSE.
+#' @param digits integer indicating the number of decimal places to round  
 #' 
 #' @details If the 'by' attribute of the input 'statRes' object is set to 'sample', then its groupvar attribute must be set to NULL
 #' 
@@ -33,7 +34,6 @@ report_dataRes<- function(dataRes, minmax = FALSE, digits = 2){
   
   if(attr(dataRes, "by") == "molecule"){
       if(!is.null(attr(dataRes, "groupvar"))){
-        
         #subsetting dataRes object
         avg = dataRes$mean
         avg[, 2:ncol(avg)]<- round(avg[,-1], digits = digits)
@@ -62,7 +62,7 @@ report_dataRes<- function(dataRes, minmax = FALSE, digits = 2){
         all_data = merge(data_mean_sd, data_min_max, by = c(edata_cname, "variable"))
         mean_sd_cols = all_data[,c("mean", "sd")]
         first_cols = data_mean_sd[, -which(names(data_mean_sd) %in% names(mean_sd_cols))]
-        new_col = apply(mean_sd_cols, 1, function(x){paste(x[1], " \u00B1 ", x[2], sep = "")})
+        new_col = apply(mean_sd_cols, 1, function(x){if(is.na(x[1]) & is.na(x[2])){paste("NA")}else{paste(x[1], " \u00B1 ", x[2], sep = "")}})
         
         res = cbind(first_cols, new_col) 
         names(res)[3]<- "value"
@@ -71,56 +71,102 @@ report_dataRes<- function(dataRes, minmax = FALSE, digits = 2){
         
         if(minmax == TRUE){
           min_max_cols = all_data[,c("min", "max")]
-          new_col2 = apply(min_max_cols, 1, function(x){paste(x[1], x[2], sep = "/")})
+          new_col2 = apply(min_max_cols, 1, function(x){if(is.na(x[1]) & is.na(x[2])){paste("NA")}else{paste(x[1], x[2], sep = "/")}})
           
-          res2 = cbind(first_cols, paste(new_col, new_col2, sep = '\n')) 
+          res2 = cbind(first_cols, paste(new_col, new_col2, sep = "<br>")) 
           names(res2)[3]<- "value"
           res2 = dcast(res2, formula = formula2)
           
           res = res2
         }
       }else{
-        #subsetting dataRes object
-        avg = dataRes$mean
-        avg$mean<- round(avg[,-which(names(avg) == edata_cname)], digits = digits)
-        
-        std_div = dataRes$sd
-        std_div$sd<- round(std_div[,-which(names(std_div) == edata_cname)], digits = digits)
-        
-        minimum = dataRes$min
-        minimum$min<- round(minimum[,-which(names(minimum) == edata_cname)], digits = digits)
-        
-        maximum = dataRes$max
-        maximum$max<- round(maximum[,-which(names(maximum) == edata_cname)], digits = digits)
-        
-        data_mean_sd<- merge(avg, std_div, by = edata_cname)
-        data_min_max = merge(minimum, maximum, by = edata_cname)
-        all_data = merge(data_mean_sd, data_min_max, by = edata_cname)
-        
-        mean_sd_cols = all_data[,c("mean", "sd")]
-        first_cols = as.data.frame(data_mean_sd[, -which(names(data_mean_sd) %in% names(mean_sd_cols))])
-        names(first_cols)[1]<- edata_cname
-        new_col = apply(mean_sd_cols, 1, function(x){paste(x[1], " \u00B1 ", x[2], sep = "")})
-        
-        res = cbind(first_cols, new_col) 
-        names(res)[2]<- "value"
-        
-        if(minmax == TRUE){
-          min_max_cols = all_data[,c("min", "max")]
-          new_col2 = apply(min_max_cols, 1, function(x){paste(x[1], x[2], sep = "/")})
-          res2 = cbind(first_cols, paste(new_col, new_col2, sep = "\n"))
-          names(res2)[2]<- "value"
-          res = res2
+        if(is.null(attr(dataRes, "groupDF"))){
+          #subsetting dataRes object
+          avg = dataRes$mean
+          avg$mean<- round(avg[,-which(names(avg) == edata_cname)], digits = digits)
+          
+          std_div = dataRes$sd
+          std_div$sd<- round(std_div[,-which(names(std_div) == edata_cname)], digits = digits)
+          
+          minimum = dataRes$min
+          minimum$min<- round(minimum[,-which(names(minimum) == edata_cname)], digits = digits)
+          
+          maximum = dataRes$max
+          maximum$max<- round(maximum[,-which(names(maximum) == edata_cname)], digits = digits)
+          
+          data_mean_sd<- merge(avg, std_div, by = edata_cname)
+          data_min_max = merge(minimum, maximum, by = edata_cname)
+          all_data = merge(data_mean_sd, data_min_max, by = edata_cname)
+          
+          mean_sd_cols = all_data[,c("mean", "sd")]
+          first_cols = as.data.frame(data_mean_sd[, -which(names(data_mean_sd) %in% names(mean_sd_cols))])
+          names(first_cols)[1]<- edata_cname
+          new_col = apply(mean_sd_cols, 1, function(x){if(is.na(x[1]) & is.na(x[2])){paste("NA")}else{paste(x[1], " \u00B1 ", x[2], sep = "")}})
+          
+          res = cbind(first_cols, new_col) 
+          names(res)[2]<- "value"
+          
+          if(minmax == TRUE){
+            min_max_cols = all_data[,c("min", "max")]
+            new_col2 = apply(min_max_cols, 1, function(x){if(is.na(x[1]) & is.na(x[2])){paste("NA")}else{paste(x[1], x[2], sep = "/")}})
+            res2 = cbind(first_cols, paste(new_col, new_col2, sep = "<br>"))
+            names(res2)[2]<- "value"
+            res = res2
+          }
+        }
+        else if(!is.null(attr(dataRes, "groupDF"))){
+          #subsetting dataRes object
+          avg = dataRes$mean
+          avg[, 2:ncol(avg)]<- round(avg[,-1], digits = digits)
+          
+          std_div = dataRes$sd
+          std_div[, 2:ncol(std_div)]<- round(std_div[, -1], digits = digits)
+          
+          minimum = dataRes$min
+          minimum[, 2:ncol(minimum)]<- round(minimum[, -1], digits = digits)
+          
+          maximum = dataRes$max
+          maximum[, 2:ncol(maximum)]<- round(maximum[, -1], digits = digits)
+          
+          #organizing data
+          mean_melt = melt(avg, id.vars = edata_cname)
+          names(mean_melt)[3]<- "mean"
+          sd_melt = melt(std_div, id.vars = edata_cname)
+          names(sd_melt)[3]<- "sd"
+          min_melt = melt(minimum, id.vars = edata_cname)
+          names(min_melt)[3]<- "min"
+          max_melt = melt(maximum, id.vars = edata_cname)
+          names(max_melt)[3]<- "max"
+          
+          data_mean_sd<- merge(mean_melt, sd_melt, by = c(edata_cname, "variable"))
+          data_min_max<- merge(min_melt, max_melt, by = c(edata_cname, "variable"))
+          all_data = merge(data_mean_sd, data_min_max, by = c(edata_cname, "variable"))
+          mean_sd_cols = all_data[,c("mean", "sd")]
+          first_cols = data_mean_sd[, -which(names(data_mean_sd) %in% names(mean_sd_cols))]
+          new_col = apply(mean_sd_cols, 1, function(x){if(is.na(x[1]) & is.na(x[2])){paste("NA")}else{paste(x[1], " \u00B1 ", x[2], sep = "")}})
+          
+          res = cbind(first_cols, new_col) 
+          names(res)[3]<- "value"
+          formula2 = paste(edata_cname, "~...", sep = "")
+          res = dcast(res, formula = formula2)
+          
+          if(minmax == TRUE){
+            min_max_cols = all_data[,c("min", "max")]
+            new_col2 = apply(min_max_cols, 1, function(x){if(is.na(x[1]) & is.na(x[2])){paste("NA")}else{paste(x[1], x[2], sep = "/")}})
+            res2 = cbind(first_cols, paste(new_col, new_col2, sep = "<br>")) 
+            names(res2)[3]<- "value"
+            res2 = dcast(res2, formula = formula2)
+            
+            res = res2
+          }
         }
       }
   }
-  
   else if(attr(dataRes, "by") == "sample"){
     #check that groupvar is NULL
     if(!is.null(attr(dataRes, "groupvar"))) stop("if 'by' attribute is set to 'sample','groupvar' attribute must be NULL")
     
     #minmax defaults to FALSE
-    
     #subsetting dataRes object and applying round function with digits argument
     avg = dataRes$mean
     avg$mean<- round(avg[,-1], digits = digits)
@@ -141,16 +187,14 @@ report_dataRes<- function(dataRes, minmax = FALSE, digits = 2){
     mean_sd_cols = all_data[,c("mean", "sd")]
     first_cols = as.data.frame(data_mean_sd[, -which(names(data_mean_sd) %in% names(mean_sd_cols))])
     names(first_cols)<- "sample"
-    new_col = apply(mean_sd_cols, 1, function(x){paste(x[1], " \u00B1 ", x[2], sep = "")})
-    
+    new_col = apply(mean_sd_cols, 1, function(x){if(is.na(x[1]) & is.na(x[2])){paste("NA")}else{paste(x[1], " \u00B1 ", x[2], sep = "")}})
     res = cbind(first_cols, new_col) 
     names(res)[2]<- paste("mean", " \u00B1 ", "sd", sep = "") 
-    
+  
     if(minmax == TRUE){
       min_max_cols = all_data[,c("min", "max")]
-      new_col2 = apply(min_max_cols, 1, function(x){paste(x[1], x[2], sep = "/")})
-      
-      res2 = cbind(first_cols, paste(new_col, new_col2, sep = "\n")) 
+      new_col2 = apply(min_max_cols, 1, function(x){if(is.na(x[1]) & is.na(x[2])){paste("NA")}else{paste(x[1], x[2], sep = "/")}})
+      res2 = cbind(first_cols, paste(new_col, new_col2, sep = "<br>")) 
       names(res2)[2]<- paste("mean", " \u00B1 ", "sd", " (min/max)", sep = "") 
       
       res = res2
