9cc14a90d539024c9e79232126ea1a2108b9aeff
I godinez <iobani.godinez@pnnl.gov>
2018-07-11 15:31:07 -0700

    removed argument from custom_sampnames function

--- R/custom_sampnames.R
+++ R/custom_sampnames.R
@@ -3,8 +3,7 @@
 #' This helper function creates custom sample names for plot data object function
 #' 
 #' @param omicsData omicsData an object of the class 'pepData', 'proData', 'metabData', or 'lipidData', usually created by \code{pepData}, \code{proData}, \code{metabData}, or \code{lipidData}, respectively.
-#' @param sample_names is an optional argument that has three options for creating custom sample names, 'option1', 'option2','option3'. 'option1' takes the first n characters of each sample name to be the new sample names. Requires 'firstx' argument to be input. 'option2' takes a range of characters from each sample name to be the new sample names. Requires 'from' and 'to' arguments be input. 'option3' takes components separated by a delimiter in each sample name to be the new sample names. Requires 'delim' and 'components' arguments be input.
-#' @param firstx is an integer specifying the first n characters to keep as the sample name
+#' @param firstn is an integer specifying the first n characters to keep as the sample name
 #' @param from is an integer specifying the start of the range of characters to keep as the sample name
 #' @param to is an integer specifying the end of the range of characters to keep as the sample name
 #' @param delim is a delimiter to separate sample name components by
@@ -16,26 +15,23 @@
 #' 
 #' 
 
-custom_sampnames = function(omicsData, sample_names = "option1", firstx = NULL, from = NULL, to = NULL, delim = NULL, components = NULL){
-  #check that sample_names argument is one of "option1", "option2", "option3"
-  if(!(sample_names %in% c("option1", "option2", "option3"))) stop("sample_names must be one of, 'option1', 'option2', or 'option3'")
-  
+custom_sampnames = function(omicsData, firstn = NULL, from = NULL, to = NULL, delim = NULL, components = NULL){
   #extract sample names from omicsData object
   fdata_cname = attr(omicsData, "cnames")$fdata_cname
   names = as.list(as.character(omicsData$f_data[[fdata_cname]]))
   
-  if(sample_names == "option1" & !is.null(firstx)){
-    if(!is.null(from) | !is.null(to) | !is.null(delim) | !is.null(components)) stop("only firstx argument is needed with 'option1'")
+  if(!is.null(firstn)){
+    if(!is.null(from) | !is.null(to) | !is.null(delim) | !is.null(components)) stop("only firstn argument is needed")
     
     output = lapply(names, function(x){temp = strsplit(x, split = "")
-                                       if(firstx > length(temp[[1]])) stop(paste("there are less than", firstx, "characters in a sample name", sep = " "))
-                                       temp2 = paste(temp[[1]][1:firstx], collapse = "")
+                                       if(firstn > length(temp[[1]])) stop(paste("there are less than", firstn, "characters in a sample name", sep = " "))
+                                       temp2 = paste(temp[[1]][1:firstn], collapse = "")
                                        return(temp2)})
     output = unlist(output)
   }
   
-  else if((sample_names == "option2") & (!is.null(from)) & (!is.null(to))){
-    if(!is.null(firstx) | !is.null(delim) | !is.null(components)) stop("only from and to arguments are needed with 'option2'")
+  else if((!is.null(from)) & (!is.null(to))){
+    if(!is.null(firstn) | !is.null(delim) | !is.null(components)) stop("only from and to arguments are needed")
     
     output = lapply(names, function(x){temp = strsplit(x, split = "")
                                        if(!(from %in% 1:length(temp[[1]])) & !(to %in% 1:length(temp[[1]]))) stop(paste(from, to, "are not in the range of the length of at least one sample name", sep = " "))
@@ -45,8 +41,8 @@ custom_sampnames = function(omicsData, sample_names = "option1", firstx = NULL, 
     output = unlist(output)
   }
   
-  else if(sample_names == "option3" & (!is.null(delim)) & (!is.null(components))){
-    if(!is.null(firstx) | !is.null(from) | !is.null(to)) stop("only delim and components arguments are needed with 'option3'")
+  else if((!is.null(delim)) & (!is.null(components))){
+    if(!is.null(firstn) | !is.null(from) | !is.null(to)) stop("only delim and components arguments are needed")
     
     output = lapply(names, function(x){temp = strsplit(x, split = delim)
                                        if(length(components) > length(temp[[1]])) stop(paste("the length of 'components vector must be less than", lenght(temp[[1]]), sep = " "))
@@ -57,7 +53,7 @@ custom_sampnames = function(omicsData, sample_names = "option1", firstx = NULL, 
     
   }
   
-  omicsData$f_data[["plot_names"]] = output
+  omicsData$f_data[["VizSampNames"]] = output
   
   return(omicsData)
 }
