7dc7a827f481177258925d88910f6b50f7685e6d
Evan A Martin <evanamartin@gmail.com>
2021-07-12 11:53:44 -0700

    Force data to be log transformed before running PCA.

--- R/dim_reduction.R
+++ R/dim_reduction.R
@@ -1,18 +1,29 @@
 #' Reduce Dimension of Data for Exploratory Data Analysis
 #'
-#' Calculate principal components using projection pursuit estimation, which implements an expectation-maximization (EM) estimation algorithm when data is missing.
+#' Calculate principal components using projection pursuit estimation, which
+#' implements an expectation-maximization (EM) estimation algorithm when data is
+#' missing.
 #'
-#' @param omicsData an object of the class 'pepdata', 'prodata', 'metabData', 'lipidData', 'nmrData' usually created by \code{\link{as.pepData}}, \code{\link{as.proData}}, \code{\link{as.metabData}}, \code{\link{as.lipidData}}, \code{\link{as.nmrData}}, respectively.
+#' @param omicsData an object of the class 'pepdata', 'prodata', 'metabData',
+#'   'lipidData', 'nmrData' usually created by \code{\link{as.pepData}},
+#'   \code{\link{as.proData}}, \code{\link{as.metabData}},
+#'   \code{\link{as.lipidData}}, \code{\link{as.nmrData}}, respectively.
 #' @param k integer number of principal components to return. Defaults to 2.
 #'
-#' @return a data.frame with first \code{k} principal component scores, sample identifiers, and group membership for each sample (if group designation was previously run on the data).
+#' @return a data.frame with first \code{k} principal component scores, sample
+#'   identifiers, and group membership for each sample (if group designation was
+#'   previously run on the data).
 #'
-#' @references Redestig, H., Stacklies, W., Scholz, M., Selbig, J., & Walther, D. (2007). \emph{pcaMethods - a bioconductor package providing PCA methods for incomplete data}. Bioinformatics. 23(9): 1164-7.
+#' @references Redestig, H., Stacklies, W., Scholz, M., Selbig, J., & Walther,
+#'   D. (2007). \emph{pcaMethods - a bioconductor package providing PCA methods
+#'   for incomplete data}. Bioinformatics. 23(9): 1164-7.
 #'
-#' @details Any biomoleculs seen in only one sample or with a variance less than 1E-6 across all samples are not included in the PCA calculations. This function leverages code from \code{\link[pcaMethods]{pca}}.
+#' @details Any biomoleculs seen in only one sample or with a variance less than
+#'   1E-6 across all samples are not included in the PCA calculations. This
+#'   function leverages code from \code{\link[pcaMethods]{pca}}.
 #' 
 #' @examples 
-#' dontrun{
+#' \dontrun{
 #' library(pmartRdata)
 #' data(lipid_object)
 #' lipid_object <- edata_transform(omicsData = lipid_object, data_scale="log2")
@@ -28,14 +39,14 @@
 #'
 dim_reduction <- function(omicsData, k = 2){
   # check that omicsData is of appropriate class #
-  if(!inherits(omicsData, c("pepData","proData","metabData", "lipidData", "nmrData"))) stop("omicsR_data must be an object of class 'pepdata','prodata', 'metabData', 'lipidData', or 'nrmData'.")
+  if(!inherits(omicsData, c("pepData","proData","metabData", "lipidData", "nmrData"))) stop("omicsData must be an object of class 'pepdata','prodata', 'metabData', 'lipidData', or 'nrmData'.")
 
   # check that group designation has been run #
   if(!("group_DF" %in% names(attributes(omicsData)))) warning("group_designation has not been run on this data and may limit plotting options")
 
   # data should be log transformed #
-  if(!attr(omicsData, "data_info")$data_scale %in% c("log2", "log10", "log")){
-    warning("omicsData$e_data should be log transformed prior to calling dim_reduction. See documentation for edata_transform function for more information.")
+  if (get_data_scale(omicsData) == "abundance") {
+    stop ("omicsData must be log transformed prior to calling dim_reduction.")
   }
 
   samp_id = attr(omicsData, "cnames")$fdata_cname
