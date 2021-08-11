b90ab2f0aabd7bb5b3ddf02df8f5b1169107fa7f
Evan A Martin <evanamartin@gmail.com>
2021-08-11 10:12:45 -0700

    Write tests for and update the rmd_conversion function

--- R/rmd_conversion.R
+++ R/rmd_conversion.R
@@ -1,37 +1,49 @@
 #' Conversion between log2(RMD) and p-value
 #'
-#' This function provides a conversion between the log base 2 robust Mahalanobis distance value and p-value for output from the \code{rmd_runs} function
+#' This function provides a conversion between the log base 2 robust Mahalanobis
+#' distance value and p-value for output from the \code{rmd_runs} function
 #'
-#' @param log2rmd numeric log base 2 transformed robust Mahalanobis distance value
+#' @param log2rmd numeric log base 2 transformed robust Mahalanobis distance
+#'   value
 #' @param pval numeric p-value associated with rmd_runs algorithm
-#' @param df integer value specifying the degrees of freedom associated with the test, which should be equal to the number of metrics used in rmd_runs
+#' @param df integer value specifying the degrees of freedom associated with the
+#'   test, which should be equal to the number of metrics used in rmd_runs
 #'
-#' @details Only one of \code{log2rmd} and \code{pval} should be provided. The input not provided will be solved for based on the provided input.
+#' @details Only one of \code{log2rmd} and \code{pval} should be provided. The
+#'   input not provided will be solved for based on the provided input.
 #'
-#' @return The function returns the corresponding p-value or log base 2 robust Mahalanobis when the other parameter is specified.
+#' @return The function returns the corresponding p-value or log base 2 robust
+#'   Mahalanobis when the other parameter is specified.
 #'
 #' @examples
-#' dontrun{
+#' \dontrun{
 #' library(pmartRdata)
 #' data(metab_object)
-#' metab_object2 <- edata_transform(omicsData = metab_object, data_scale = "log2")
-#' metab_object3 <- group_designation(omicsData = metab_object2, main_effects = "Condition")
-#' rmd_results <- rmd_filter(omicsData = metab_object3, metrics=c("MAD", "Skewness", "Correlation"))
+#' metab_object2 <- edata_transform(omicsData = metab_object,
+#'                                  data_scale = "log2")
+#' metab_object3 <- group_designation(omicsData = metab_object2,
+#'                                    main_effects = "Condition")
+#' rmd_results <- rmd_filter(omicsData = metab_object3,
+#'                           metrics=c("MAD", "Skewness", "Correlation"))
 #' rmd_conversion(log2rmd = rmd_results$Log2.md, df=3)
 #'
 #' rmd_conversion(pval = .0001, df = 3)
 #' rmd_conversion(log2rmd = 4.5, df = 3)
-#'}
+#' }
+#'
 #' @author Lisa Bramer
+#'
 #' @export
-
-rmd_conversion <- function(log2rmd = NULL, pval = NULL, df){
+#' 
+rmd_conversion <- function (log2rmd = NULL, pval = NULL, df) {
 
   # check that only one of the two log2rmd and pval is provided #
-  if(!is.null(log2rmd) & !is.null(pval)) stop("Too many arguments entered to solve for log2rmd or pval.")
+  if(!is.null(log2rmd) & !is.null(pval))
+    stop("Too many arguments entered to solve for log2rmd or pval.")
 
   # check that one of the two log2rmd and pval is provided #
-  if(is.null(log2rmd) & is.null(pval)) stop("One of log2rmd and pval must be provided")
+  if(is.null(log2rmd) & is.null(pval))
+    stop("One of log2rmd and pval must be provided")
 
   # check that df is at least 1 #
   if(df < 1) stop("df must be an integer greater than or equal to 1.")
