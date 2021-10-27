57be6c4a9be016f4219915409d16bb5d65bdc5a1
Daniel Claborne <dmclaborne@gmail.com>
2021-10-27 10:25:39 -0700

    default check for valid f_meta among f_datas

--- R/helper_fn.R
+++ R/helper_fn.R
@@ -1017,9 +1017,16 @@ column_matches_exact <- function(df, col) {
   return(matched_cnames)
 }
 
-#' Custom message function to pretty-print text with newlines so you can follow
+#' Custom message functions to pretty-print text with newlines so you can follow
 #' character limit guidelines in source code.
-#' @noRd
 wrap_message <- function(..., prefix = " ", initial = ""){
   message(strwrap(..., prefix = prefix, initial = initial))
+}
+
+wrap_stop <- function(..., prefix = " ", initial = ""){
+  stop(strwrap(..., prefix = prefix, initial = initial))
+}
+
+wrap_warning <- function(..., prefix = " ", initial = ""){
+  warning(strwrap(..., prefix = prefix, initial = initial))
 }
