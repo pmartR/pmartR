bec7623f5efbde1f9981042ba1a9f445b6cbce5c
Evan A Martin <evanamartin@gmail.com>
2021-11-22 11:54:10 -0800

    Combine p-value adjustment functions in one file

--- R/imd_anova.R
+++ R/imd_anova.R
@@ -1300,6 +1300,12 @@ p_adjustment_anova <- function(p_values = NULL,
   return(pvalues=data.frame(adjusted_pvals))
 }
 
+# I haven't found a function that quickly gets the rank of the original p-values
+# so do it in R for now, still much faster than a pure R version
+ranked_holm_cpp <- function(ps){
+  return(holm_cpp(ps)[rank(ps)])
+}
+
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 # COVARIATE FUNCTIONS ----------------------------------------------------------
--- R/support_p_adjustment.R
+++
@@ -1,5 +0,0 @@
-#I haven't found a function that quickly gets the rank of the original p-values so do it in R for now,
-#still much faster than a pure R version
-ranked_holm_cpp <- function(ps){
-  return(holm_cpp(ps)[rank(ps)])
-}
