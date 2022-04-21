0224e6e53259346743d49bc3ab3e718e9bb55595
Evan A Martin <evanamartin@gmail.com>
2022-04-21 09:03:27 -0700

    Update omicsData summary function to include covariates and pairs and create unit tests

--- R/summary_pmartR.R
+++ R/summary_pmartR.R
@@ -27,51 +27,10 @@
 #'
 summary.pepData <- function(omicsData) {
 
-  # get values #
-  res <- list(class = class(omicsData), num_samps = attr(omicsData, "data_info")$num_samps,
-              num_edata = attr(omicsData, "data_info")$num_edata,
-              num_emeta = attr(omicsData, "meta_info")$num_emeta,
-              num_miss_obs = attr(omicsData, "data_info")$num_miss_obs,
-              prop_missing = round(attr(omicsData, "data_info")$prop_missing, 3))
+  # Army captain, "Nooooo! ... General, the enemy is about to overtake us!"
+  # Army general, "CALL IN THE SUMMARIZER!!!"
+  summarizer(omicsData)
 
-  # construct output #
-  newres <- lapply(res, function(x) ifelse(is.null(x), "NA", as.character(x)))
-  catmat <- data.frame(unlist(newres, use.names = FALSE))
-
-  # assemble text strings #
-  edata_name <- paste("Unique ", attributes(omicsData)$cnames$edata_cname, "s (e_data)", sep="")
-  fdata_name <- paste("Unique ", attributes(omicsData)$cnames$fdata_cname, "s (f_data)", sep="")
-  if(!is.null(attributes(omicsData)$cnames$emeta_cname)){
-    emeta_name <- paste("Unique ", attributes(omicsData)$cnames$emeta_cname, "s (e_meta)", sep="")
-  }else{
-    emeta_name <- "Rows (e_meta) "
-  }
-
-  colnames(catmat) <- NULL
-  rownames(catmat) <- c("Class", fdata_name, edata_name, emeta_name, "Missing Observations ", "Proportion Missing ")
-
-  #if group_DF attr is present
-  if(!is.null(attr(omicsData, "group_DF"))){
-    group_vec<- attr(omicsData, "group_DF")$Group
-    levels<- unique(attr(omicsData, "group_DF")$Group)
-    counts <- vector(mode="numeric", length=length(levels))
-
-    for(i in 1:length(levels)){
-      counts[i]<- length(which(group_vec == levels[i]))
-    }
-    res2<- as.list(counts)
-    names(res2)<- levels
-    newres2 <- lapply(res2, function(x) ifelse(is.null(x), "NA", as.character(x)))
-
-    newres2<- c(newres, newres2)
-    catmat2 <- data.frame(unlist(newres2, use.names = FALSE))
-    colnames(catmat2) <- NULL
-    rownames(catmat2) <- c("Class",fdata_name, edata_name, emeta_name, "Missing Observations ", "Proportion Missing ", paste("Samples per group:", levels, sep = " "))
-
-    catmat<- catmat2
-
-  }
-  return(catmat)
 }
 
 #'@export
@@ -79,51 +38,10 @@ summary.pepData <- function(omicsData) {
 #'@name summary-omicsData
 summary.proData <- function(omicsData) {
 
-  # get values #
-  res <- list(class = class(omicsData), num_samps = attr(omicsData, "data_info")$num_samps,
-              num_edata = attr(omicsData, "data_info")$num_edata,
-              num_emeta = attr(omicsData, "meta_info")$num_emeta,
-              num_miss_obs = attr(omicsData, "data_info")$num_miss_obs,
-              prop_missing = round(attr(omicsData, "data_info")$prop_missing, 3))
+  # Army captain, "Nooooo! ... General, the enemy is about to overtake us!"
+  # Army general, "CALL IN THE SUMMARIZER!!!"
+  summarizer(omicsData)
 
-  # construct output #
-  newres <- lapply(res, function(x) ifelse(is.null(x), "NA", as.character(x)))
-  catmat <- data.frame(unlist(newres, use.names = FALSE))
-
-  # assemble text strings #
-  edata_name <- paste("Unique ", attributes(omicsData)$cnames$edata_cname, "s (e_data)", sep="")
-  fdata_name <- paste("Unique ", attributes(omicsData)$cnames$fdata_cname, "s (f_data)", sep="")
-  if(!is.null(attributes(omicsData)$cnames$emeta_cname)){
-    emeta_name <- paste("Unique ", attributes(omicsData)$cnames$emeta_cname, "s (e_meta)", sep="")
-  }else{
-    emeta_name <- "Rows (e_meta) "
-  }
-
-  colnames(catmat) <- NULL
-  rownames(catmat) <- c("Class", fdata_name, edata_name, emeta_name, "Missing Observations ", "Proportion Missing ")
-
-  #if group_DF attr is present
-  if(!is.null(attr(omicsData, "group_DF"))){
-    group_vec<- attr(omicsData, "group_DF")$Group
-    levels<- unique(attr(omicsData, "group_DF")$Group)
-    counts <- vector(mode="numeric", length=length(levels))
-
-    for(i in 1:length(levels)){
-      counts[i]<- length(which(group_vec == levels[i]))
-    }
-    res2<- as.list(counts)
-    names(res2)<- levels
-    newres2 <- lapply(res2, function(x) ifelse(is.null(x), "NA", as.character(x)))
-
-    newres2<- c(newres, newres2)
-    catmat2 <- data.frame(unlist(newres2, use.names = FALSE))
-    colnames(catmat2) <- NULL
-    rownames(catmat2) <- c("Class", fdata_name, edata_name, emeta_name, "Missing Observations ", "Proportion Missing ", paste("Samples per group:", levels, sep = " "))
-
-    catmat<- catmat2
-  }
-
-  return(catmat)
 }
 
 
@@ -132,51 +50,10 @@ summary.proData <- function(omicsData) {
 #'@name summary-omicsData
 summary.lipidData <- function(omicsData) {
 
-  # get values #
-  res <- list(class = class(omicsData), num_samps = attr(omicsData, "data_info")$num_samps,
-              num_edata = attr(omicsData, "data_info")$num_edata,
-              num_emeta = attr(omicsData, "meta_info")$num_emeta,
-              num_miss_obs = attr(omicsData, "data_info")$num_miss_obs,
-              prop_missing = round(attr(omicsData, "data_info")$prop_missing, 3))
+  # Army captain, "Nooooo! ... General, the enemy is about to overtake us!"
+  # Army general, "CALL IN THE SUMMARIZER!!!"
+  summarizer(omicsData)
 
-  # construct output #
-  newres <- lapply(res, function(x) ifelse(is.null(x), "NA", as.character(x)))
-  catmat <- data.frame(unlist(newres, use.names = FALSE))
-
-  # assemble text strings #
-  edata_name <- paste("Unique ", attributes(omicsData)$cnames$edata_cname, "s (e_data)", sep="")
-  fdata_name <- paste("Unique ", attributes(omicsData)$cnames$fdata_cname, "s (f_data)", sep="")
-  if(!is.null(attributes(omicsData)$cnames$emeta_cname)){
-    emeta_name <- paste("Unique ", attributes(omicsData)$cnames$emeta_cname, "s (e_meta)", sep="")
-  }else{
-    emeta_name <- "Rows (e_meta) "
-  }
-
-  colnames(catmat) <- NULL
-  rownames(catmat) <- c("Class", fdata_name, edata_name, emeta_name, "Missing Observations ", "Proportion Missing ")
-
-  #if group_DF attr is present
-  if(!is.null(attr(omicsData, "group_DF"))){
-    group_vec<- attr(omicsData, "group_DF")$Group
-    levels<- unique(attr(omicsData, "group_DF")$Group)
-    counts <- vector(mode="numeric", length=length(levels))
-
-    for(i in 1:length(levels)){
-      counts[i]<- length(which(group_vec == levels[i]))
-    }
-    res2<- as.list(counts)
-    names(res2)<- levels
-    newres2 <- lapply(res2, function(x) ifelse(is.null(x), "NA", as.character(x)))
-
-    newres2<- c(newres, newres2)
-    catmat2 <- data.frame(unlist(newres2, use.names = FALSE))
-    colnames(catmat2) <- NULL
-    rownames(catmat2) <- c("Class", fdata_name, edata_name, emeta_name, "Missing Observations ", "Proportion Missing ", paste("Samples per group:", levels, sep = " "))
-
-    catmat<- catmat2
-  }
-
-  return(catmat)
 }
 
 
@@ -186,51 +63,10 @@ summary.lipidData <- function(omicsData) {
 #'@name summary-omicsData
 summary.metabData <- function(omicsData) {
 
-  # get values #
-  res <- list(class = class(omicsData), num_samps = attr(omicsData, "data_info")$num_samps,
-              num_edata = attr(omicsData, "data_info")$num_edata,
-              num_emeta = attr(omicsData, "meta_info")$num_emeta,
-              num_miss_obs = attr(omicsData, "data_info")$num_miss_obs,
-              prop_missing = round(attr(omicsData, "data_info")$prop_missing, 3))
+  # Army captain, "Nooooo! ... General, the enemy is about to overtake us!"
+  # Army general, "CALL IN THE SUMMARIZER!!!"
+  summarizer(omicsData)
 
-  # construct output #
-  newres <- lapply(res, function(x) ifelse(is.null(x), "NA", as.character(x)))
-  catmat <- data.frame(unlist(newres, use.names = FALSE))
-
-  # assemble text strings #
-  edata_name <- paste("Unique ", attributes(omicsData)$cnames$edata_cname, "s (e_data)", sep="")
-  fdata_name <- paste("Unique ", attributes(omicsData)$cnames$fdata_cname, "s (f_data)", sep="")
-  if(!is.null(attributes(omicsData)$cnames$emeta_cname)){
-    emeta_name <- paste("Unique ", attributes(omicsData)$cnames$emeta_cname, "s (e_meta)", sep="")
-  }else{
-    emeta_name <- "Rows (e_meta) "
-  }
-
-  colnames(catmat) <- NULL
-  rownames(catmat) <- c("Class", fdata_name, edata_name, emeta_name, "Missing Observations ", "Proportion Missing ")
-
-  #if group_DF attr is present
-  if(!is.null(attr(omicsData, "group_DF"))){
-    group_vec<- attr(omicsData, "group_DF")$Group
-    levels<- unique(attr(omicsData, "group_DF")$Group)
-    counts <- vector(mode="numeric", length=length(levels))
-
-    for(i in 1:length(levels)){
-      counts[i]<- length(which(group_vec == levels[i]))
-    }
-    res2<- as.list(counts)
-    names(res2)<- levels
-    newres2 <- lapply(res2, function(x) ifelse(is.null(x), "NA", as.character(x)))
-
-    newres2<- c(newres, newres2)
-    catmat2 <- data.frame(unlist(newres2, use.names = FALSE))
-    colnames(catmat2) <- NULL
-    rownames(catmat2) <- c("Class", fdata_name, edata_name, emeta_name, "Missing Observations ", "Proportion Missing ", paste("Samples per group:", levels, sep = " "))
-
-    catmat<- catmat2
-  }
-
-  return(catmat)
 }
 
 
@@ -239,49 +75,173 @@ summary.metabData <- function(omicsData) {
 #'@name summary-omicsData
 summary.nmrData <- function(omicsData) {
 
-  # get values #
-  res <- list(class = class(omicsData), num_samps = attr(omicsData, "data_info")$num_samps,
-              num_edata = attr(omicsData, "data_info")$num_edata,
-              num_emeta = attr(omicsData, "meta_info")$num_emeta,
-              num_miss_obs = attr(omicsData, "data_info")$num_miss_obs,
-              prop_missing = round(attr(omicsData, "data_info")$prop_missing, 3))
+  # Army captain, "Nooooo! ... General, the enemy is about to overtake us!"
+  # Army general, "CALL IN THE SUMMARIZER!!!"
+  summarizer(omicsData)
 
-  # construct output #
-  newres <- lapply(res, function(x) ifelse(is.null(x), "NA", as.character(x)))
-  catmat <- data.frame(unlist(newres, use.names = FALSE))
+}
 
-  # assemble text strings #
-  edata_name <- paste("Unique ", attributes(omicsData)$cnames$edata_cname, "s (e_data)", sep="")
-  fdata_name <- paste("Unique ", attributes(omicsData)$cnames$fdata_cname, "s (f_data)", sep="")
-  if(!is.null(attributes(omicsData)$cnames$emeta_cname)){
-    emeta_name <- paste("Unique ", attributes(omicsData)$cnames$emeta_cname, "s (e_meta)", sep="")
-  }else{
-    emeta_name <- "Rows (e_meta) "
-  }
+# A Neat little function that summarizes an omicsData object. This function adds
+# covariate and pair counts to the original summary function.
+#
+# @author Evan A Martin
+summarizer <- function (omicsData) {
 
-  colnames(catmat) <- NULL
-  rownames(catmat) <- c("Class", fdata_name, edata_name, emeta_name, "Missing Observations ", "Proportion Missing ")
+  # The following values will always be present no matter what. They wont change
+  # depending on the main effects, covariates, and whether the data are paired.
+  # For the remainder of the script we will append values to this list.
+  res <- list(
+    class = class(omicsData),
+    num_samps = attr(omicsData, "data_info")$num_samps,
+    num_edata = attr(omicsData, "data_info")$num_edata,
+    num_emeta = attr(omicsData, "meta_info")$num_emeta,
+    num_miss_obs = attr(omicsData, "data_info")$num_miss_obs,
+    prop_missing = round(attr(omicsData, "data_info")$prop_missing, 3)
+  )
 
-  #if group_DF attr is present
-  if(!is.null(attr(omicsData, "group_DF"))){
-    group_vec<- attr(omicsData, "group_DF")$Group
-    levels<- unique(attr(omicsData, "group_DF")$Group)
-    counts <- vector(mode="numeric", length=length(levels))
+  # Create a row names vector that matches the elements of res. Because res will
+  # always have the elements above the row names vector will also have a name
+  # for each of those elements. This will be the base vector. Additional
+  # elements will be appended to the row names vector depending on main effects,
+  # covariates, and whether the data are paired.
+  res_names <- c(
+    "Class",
+    paste("Unique ", get_edata_cname(omicsData), "s (e_data)", sep = ""),
+    paste("Unique ", get_fdata_cname(omicsData), "s (f_data)", sep = ""),
+    if (is.null(get_emeta_cname(omicsData)))
+      "Rows (e_meta)" else
+        paste("Unique ", get_emeta_cname(omicsData), "s (e_meta)", sep = ""),
+    "Missing Observations",
+    "Proportion Missing"
+  )
 
-    for(i in 1:length(levels)){
-      counts[i]<- length(which(group_vec == levels[i]))
+  # Check if the data are grouped.
+  if (!is.null(attr(omicsData, "group_DF"))) {
+
+    # Add group counts ---------------
+
+    group_vec <- attr(omicsData, "group_DF")$Group
+    levels <- unique(group_vec)
+    counts <- vector(mode = "list", length = length(levels))
+
+    for (i in 1:length(levels)) {
+
+      # Determine the number of samples in the current group.
+      counts[[i]] <- length(which(group_vec == levels[[i]]))
+
     }
-    res2<- as.list(counts)
-    names(res2)<- levels
-    newres2 <- lapply(res2, function(x) ifelse(is.null(x), "NA", as.character(x)))
 
-    newres2<- c(newres, newres2)
-    catmat2 <- data.frame(unlist(newres2, use.names = FALSE))
-    colnames(catmat2) <- NULL
-    rownames(catmat2) <- c("Class", fdata_name, edata_name, emeta_name, "Missing Observations ", "Proportion Missing ", paste("Samples per group:", levels, sep = " "))
+    # Name the counts with their corresponding group names.
+    names(counts) <- levels
 
-    catmat<- catmat2
+    # Add the group counts to res.
+    res <- c(res, counts)
+
+    # Update the names of res depening on the main effects of the groups. For
+    # example, if the data are paired and there are no main effects we don't
+    # want to print the number of samples in the paired_diff group. (We want to
+    # keep this group a secret from the user. Shhhhhhh!)
+    if ("paired_diff" %in% levels) {
+
+      res_names <- c(res_names, "Total Samples")
+
+    } else {
+
+      # Add each group name to the res_names vector.
+      res_names <- c(res_names, paste("Samples per group:", levels, sep = " "))
+
+    }
+
+    # Add covariate counts ---------------
+
+    # Check for covariates. If they exist we will tell the user how many.
+    if (!is.null(attr(get_group_DF(omicsData), "covariates"))) {
+
+      # Nab the covariate data frame. This will be used later for sundry
+      # purposes.
+      covies <- attr(get_group_DF(omicsData), "covariates")
+
+      # Determine the number of covariates. This is the number of columns in the
+      # covariates attribute minus one (because of the sample name column).
+      n_cov <- dim(covies)[[2]] - 1
+
+      # Loop through each covariate, check its class (e.g., numeric, character,
+      # factor), and count the number of samples in each level.
+      for (e in 1:n_cov) {
+
+        # Check if the covariate is categorical (i.e., the class is character or
+        # factor).
+        if (is.character(covies[, e + 1]) || is.factor(covies[, e + 1])) {
+
+          # Count the number of levels in the current covariate and create a
+          # list to hold the number of samples in each level.
+          cov_level <- as.character(unique(covies[, e + 1]))
+          level_count <- vector(mode = "list", length = length(cov_level))
+
+          # Loop through each level and count the number of samples.
+          for (v in 1:length(cov_level)) {
+
+            # Determine the number of samples in the current level.
+            level_count[[v]] <- length(which(covies[, e + 1] == cov_level[[v]]))
+
+          }
+
+          # Name the counts with their corresponding covariate/level names.
+          names(level_count) <- paste(names(covies)[e + 1],
+                                      cov_level,
+                                      sep = "_")
+
+          # Runs when the covariate is numeric.
+        } else {
+
+          # The covariate is continuous: We will just report the name of the
+          # covaraite along with the total number of samples for this covariate.
+          level_count <- list(nrow(covies))
+          names(level_count) <- names(covies)[e + 1]
+
+        }
+
+        # Add the covariate counts to res.
+        res <- c(res, level_count)
+
+        # Add the name of the covariate levels to res_names.
+        res_names <- c(res_names,
+                       paste("Samples per covariate:",
+                             names(level_count), sep = " "))
+
+      }
+
+    }
+
+    # Add pair counts ------------
+
+    # Check if the data are paired. If they are the number of pairs will be
+    # added to the beautiful summary we are creating!
+    if (!is.null(attr(get_group_DF(omicsData), "pairs"))) {
+
+      # Nab the name of the pair variable. This will be used to subset f_data.
+      pair_name <- attr(get_group_DF(omicsData), "pairs")
+
+      # Add the number of pairs to res.
+      res <- c(res,
+               list(pairs = dplyr::n_distinct(omicsData$f_data[, pair_name])))
+
+      # Update the names vector with the row name for the number of pairs.
+      res_names <- c(res_names, "Pairs")
+
+    }
+
   }
 
-  return(catmat)
+  # Convert all elements in res to a character.
+  res <- lapply(res, function(x) ifelse(is.null(x), "NA", as.character(x)))
+
+  # Turn res into a data frame, remove all column names, and include the
+  # established row names.
+  catmat <- data.frame(unlist(res, use.names = FALSE))
+  colnames(catmat) <- NULL
+  rownames(catmat) <- res_names
+
+  return (catmat)
+
 }
---
+++ tests/testthat/test_summary_omicsData.R
@@ -0,0 +1,129 @@
+context('summary: omicsData')
+
+test_that('the omicsData summaries are all square',{
+
+  # Create pepData objects to test with and without group info -----------------
+
+  load(system.file('testdata',
+                   'little_pdata.RData',
+                   package = 'pmartR'))
+
+  pdata <- as.pepData(e_data = edata,
+                      f_data = fdata,
+                      e_meta = emeta,
+                      edata_cname = 'Mass_Tag_ID',
+                      fdata_cname = 'SampleID',
+                      emeta_cname = 'Protein')
+
+  pdata_gdf <- group_designation(omicsData = pdata,
+                                 main_effects = 'Condition')
+
+  # Create pepData objects to test multiple main effects and covariates --------
+
+  # Copy edata so the names of the samples can be changed.
+  edata2_2 <- edata
+
+  # Change some of the Infection samples to Mock samples.
+  names(edata2_2) <- c("Mass_Tag_ID",
+                       paste0("Infection", 1:6),
+                       paste0("Mock", 1:6))
+
+  # Create additional f_data objects with different main effects and covariates.
+  fdata2_2 <- fdata
+
+  # Update the sample names in f_data.
+  fdata2_2$SampleID <- c(paste0("Infection", 1:6),
+                         paste0("Mock", 1:6))
+
+  # Update the first main effect to account for changing some infection samples to
+  # mock samples.
+  fdata2_2$Condition <- c(rep("Infection", 6),
+                          rep("Mock", 6))
+
+  fdata2_2$Level <- c("high", "low", "high", "low", "high", "low", "high",
+                      "high", "low", "low", "low", "high")
+  fdata2_2$Gender <- c("M", "F", "M", "F", "M", "F",
+                       "F", "M", "F", "F", "M", "F")
+  fdata2_2$Age <- round(runif(12, min = 19, max = 89), 2)
+
+  pdata_2_2_4 <- as.pepData(e_data = edata2_2,
+                            f_data = fdata2_2,
+                            edata_cname = "Mass_Tag_ID",
+                            fdata_cname = "SampleID")
+  pdata_2_2_4 <- group_designation(omicsData = pdata_2_2_4,
+                                   main_effects = c("Condition", "Level"),
+                                   covariates = c("Gender", "Age"))
+
+  # Create pepData objects to test paired data ---------------------------------
+
+  load(system.file('testdata',
+                   'little_pairdata.RData',
+                   package = 'pmartR'))
+
+  pairdata <- as.pepData(e_data = edata,
+                         f_data = fdata,
+                         e_meta = emeta,
+                         edata_cname = 'Mass_Tag_ID',
+                         fdata_cname = 'Name',
+                         emeta_cname = 'Protein')
+
+  pairdata <- group_designation(pairdata,
+                                pairs = "PairID")
+
+  # Carpe unitae testum --------------------------------------------------------
+
+  expect_identical(
+    summary(pdata),
+    data.frame(
+      temp = c("pepData", "12", "150", "83", "341", "0.189"),
+      row.names = c("Class", "Unique Mass_Tag_IDs (e_data)",
+                    "Unique SampleIDs (f_data)", "Unique Proteins (e_meta)",
+                    "Missing Observations", "Proportion Missing")
+    ) %>%
+      `names<-`(NULL)
+  )
+
+  expect_identical(
+    summary(pdata_gdf),
+    data.frame(
+      temp = c("pepData", "12", "150", "83", "341", "0.189", "9", "3"),
+      row.names = c("Class", "Unique Mass_Tag_IDs (e_data)",
+                    "Unique SampleIDs (f_data)", "Unique Proteins (e_meta)",
+                    "Missing Observations", "Proportion Missing",
+                    "Samples per group: Infection", "Samples per group: Mock")
+    ) %>%
+      `names<-`(NULL)
+  )
+
+  expect_identical(
+    summary(pdata_2_2_4),
+    data.frame(
+      temp = c("pepData", "12", "150", "NA", "341", "0.189",
+               "3", "3", "3", "3", "5", "7", "12"),
+      row.names = c("Class", "Unique Mass_Tag_IDs (e_data)",
+                    "Unique SampleIDs (f_data)", "Rows (e_meta)",
+                    "Missing Observations", "Proportion Missing",
+                    "Samples per group: Infection_high",
+                    "Samples per group: Infection_low",
+                    "Samples per group: Mock_high",
+                    "Samples per group: Mock_low",
+                    "Samples per covariate: Gender_M",
+                    "Samples per covariate: Gender_F",
+                    "Samples per covariate: Age")
+    ) %>%
+      `names<-`(NULL)
+  )
+
+  expect_identical(
+    summary(pairdata),
+    data.frame(
+      temp = c("pepData", "30", "150", "16", "1184", "0.263", "30", "15"),
+      row.names = c("Class", "Unique Mass_Tag_IDs (e_data)",
+                    "Unique Names (f_data)", "Unique Proteins (e_meta)",
+                    "Missing Observations", "Proportion Missing",
+                    "Total Samples", "Pairs")
+    ) %>%
+      `names<-`(NULL)
+  )
+
+})
