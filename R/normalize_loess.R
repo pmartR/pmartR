d1c1e870dfaa7f3f327f30cd309f9fc2d4f14e0c
Richardson, Rachel E <rachel.richardson@pnnl.gov>
2022-08-30 13:53:10 -0700

    rebase, small changes with testing

--- R/normalize_loess.R
+++ R/normalize_loess.R
@@ -26,10 +26,14 @@
 #' @export
 normalize_loess<- function(omicsData, method = "fast", span = .4){
   # check that omicsData is of the appropriate class
-  if(!inherits(omicsData, c("proData","pepData","lipidData", "metabData", "nmrData"))) stop("omicsData is not an object of appropriate class")
+  if(!inherits(omicsData, c("proData","pepData","lipidData", "metabData", "nmrData"))){
+    stop("omicsData must be of class 'pepData', 'proData', 'lipidData', 'metabData' or 'nmrData'")
+  } 
   
   # data should be on log scale #
-  if(!(attr(omicsData, "data_info")$data_scale %in% c("log2", "log10", "log"))) stop("omicsData must be on log scale")
+  if(!(attr(omicsData, "data_info")$data_scale %in% c("log2", "log10", "log"))){
+    stop("omicsData must be on log scale")
+  }
 
   #check that norm attribute is False
   if(get_data_norm(omicsData)) stop("data has already been normalized")
