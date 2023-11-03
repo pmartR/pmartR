56bfd3bd903e3a7f36038842553b9c63fac6044e
Daniel Claborne <dmclaborne@gmail.com>
2023-11-03 13:28:01 -0700

    remove pmartRdata reference from tests
    skip plot tests on CRAN, ignore vignettes in build

--- tests/testthat/test_plot_errors.R
+++ tests/testthat/test_plot_errors.R
@@ -6,12 +6,25 @@ test_that('plotting errors are triggered correctly', {
     package = 'pmartR'
   ))
 
-
   myseqData <- as.seqData(
     e_data = edata,
     f_data = fdata,
     edata_cname = 'ID_REF',
     fdata_cname = 'Samples'
+  )
+
+  load(system.file('testdata',
+    'little_isodata.RData',
+    package = 'pmartR'
+  ))
+
+  isodata <- as.isobaricpepData(
+    e_data = edata,
+    f_data = fdata,
+    e_meta = emeta,
+    edata_cname = 'Peptide',
+    fdata_cname = 'Sample',
+    emeta_cname = 'Protein'
   )
 
   err_1 <- "lkpm is not a valid option for 'transformation'"
@@ -21,5 +34,5 @@ test_that('plotting errors are triggered correctly', {
   testthat::expect_error(plot(myseqData, transformation = "log"), err_2)
   
   # "unused argument(s): lcpm"
-  testthat::expect_warning(plot(pmartRdata::isobaric_object, transformation = "lcpm")) 
+  testthat::expect_warning(plot(isodata, transformation = "lcpm")) 
 })
